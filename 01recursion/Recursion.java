7e92a6e5728aea233953ef4053d380fc4c2a5840
Roy Kim <rkim8@stuy.edu>
2016-02-04 18:03:03 -0500

    Wrote guess method

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -3,5 +3,15 @@ public class Recursion implements hw01{
 	return "Kim,Roy";
     }
 
+    public double sqrt(double n){
+	
+    }
 
+    public double guess(double n, double guess){
+	if (n/guess == guess){
+	    return n;
+	} else {
+	    guess(n, (n/guess + guess)/2);
+	}
+    }
 } 
