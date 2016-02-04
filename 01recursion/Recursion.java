c90cbd2ec2de6802fc557851122a6fed472fe951
Roy Kim <rkim8@stuy.edu>
2016-02-04 18:10:02 -0500

    Began the sqrt method

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -4,14 +4,18 @@ public class Recursion implements hw01{
     }
 
     public double sqrt(double n){
-	
+	if (n < 0){
+	    throw new IllegalArgumentException("Non-negatives please!");
+	} else {
+	    return guess(n, n/2);
+	}
     }
 
     public double guess(double n, double guess){
 	if (n/guess == guess){
 	    return n;
 	} else {
-	    guess(n, (n/guess + guess)/2);
+	    return guess(n, (n/guess + guess)/2);
 	}
     }
 } 
