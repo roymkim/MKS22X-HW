491ad97190efbf60eae27ee6b151a1ebb81d525d
Roy Kim <rkim8@stuy.edu>
2016-02-04 20:56:25 -0500

    Completed sqrt method

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -6,14 +6,16 @@ public class Recursion implements hw01{
     public double sqrt(double n){
 	if (n < 0){
 	    throw new IllegalArgumentException("Non-negatives please!");
+	} else if (n==0) {
+	    return 0;
 	} else {
 	    return guess(n, n/2);
 	}
     }
 
     public double guess(double n, double guess){
-	if (n/guess == guess){
-	    return n;
+       	if (Math.abs(((n/guess) - guess)/guess) <= 1.0e-15){ //it seems like 1e-15 is the smallest number possible
+	    return guess;
 	} else {
 	    return guess(n, (n/guess + guess)/2);
 	}
