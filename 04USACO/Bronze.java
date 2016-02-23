359dba461b5caa49fd03765de1223a9c1ae2eba1
Roy Kim <rkim8@stuy.edu>
2016-02-23 03:23:03 -0500

    Started Bronze USACO 08

---
+++ 04USACO/Bronze.java
@@ -0,0 +1,11 @@
+public class Bronze{
+    public void stomp(int row, int col, int elevation){
+	
+    }
+
+    public int calculateElevation(){
+    }
+
+    public static void main(String[]args){
+    }
+}
