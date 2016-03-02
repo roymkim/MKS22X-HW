5bfdc1af1f0e32a47b144fd16617fb8f8641756b
Roy Kim <rkim8@stuy.edu>
2016-03-02 08:43:55 -0500

    Completed

--- 04USACO/Silver.java
+++ 04USACO/Silver.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 public class Silver{
     int N, M, T;
-    char[][] pasture;
+    int[][] pasture;
     int R1, C1, R2, C2;
 
     public Silver(String filename){
@@ -13,17 +13,21 @@ public class Silver{
 	    T = sc.nextInt();
 	    String field= "";
 	    sc.nextLine();
-	    pasture = new char[N][M];
+	    pasture = new int[N][M];
 	    for (int i = 0; i < N; i++){
 		field += sc.nextLine();
 	    }
-	    R1 = sc.nextInt();
-	    C1 = sc.nextInt();
-	    R2 = sc.nextInt();
-	    C2 = sc.nextInt();
+	    R1 = sc.nextInt() - 1; //adjusting coordinates
+	    C1 = sc.nextInt() - 1;
+	    R2 = sc.nextInt() - 1;
+	    C2 = sc.nextInt() - 1;
 	    for (int r = 0; r < pasture.length; r++){
 		for (int c = 0; c < pasture[r].length; c++){
-		    pasture[r][c] = field.charAt(r*pasture[r].length+c);
+		    if (field.charAt(r*pasture[r].length+c) == '*'){
+			pasture[r][c] = -1;
+		    } else {
+			pasture[r][c] = 0;
+		    }
 		}
 	    }
         } catch (FileNotFoundException e){
@@ -31,17 +35,44 @@ public class Silver{
         }
     }
 
-    public void solve(){
+    public int solve(){
+	pasture[R1][C1] = 1;
+	for (int t = 0; t < T; t++){
+	    int[][] temp = new int[N][M];
+	    int sum = 0;
+	    for (int r = 0; r < N; r++){
+		for (int c = 0; c < M; c++){
+		    sum = 0;
+		    if (r-1 >= 0 && pasture[r-1][c] > 0) {
+			sum += pasture[r-1][c];
+		    }
+		    if (r+1 < pasture.length && pasture[r+1][c] > 0){
+			sum += pasture[r+1][c];
+		    }
+		    if (c-1 >= 0 && pasture[r][c-1] > 0){
+			sum += pasture[r][c-1];
+		    }
+		    if (c+1 < pasture[r].length && pasture[r][c+1] > 0){
+			sum += pasture[r][c+1];
+		    }
+		    if (pasture[r][c] >= 0){
+			temp[r][c] = sum;
+		    } else {
+			temp[r][c] = -1;
+		    }
+		}
+	    }
+       	    pasture = temp;
+	}
+	return pasture[R2][C2];
     }
 
-    public void solveH(){
-    }
 
     public String toString(){
 	String ans = "";
 	for (int r = 0; r < pasture.length; r++){
 	    for (int c = 0; c < pasture[r].length; c++){
-		ans+=pasture[r][c];
+		ans+=pasture[r][c]+"\t";
 	    }
 	    ans+="\n";
 	}
@@ -51,5 +82,6 @@ public class Silver{
     public static void main(String[]args){
 	Silver ctravel = new Silver("ctravel.in");
 	System.out.println(ctravel.toString());
+      	System.out.println("" + ctravel.solve() + ",7,Kim,Roy");
     }
 }
