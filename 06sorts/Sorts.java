bbc460c59e813893f454f230994ec3bf319c0320
Roy Kim <rkim8@stuy.edu>
2016-03-06 00:53:23 -0500

    Completed mergeSort

--- 06sorts/Sorts.java
+++ 06sorts/Sorts.java
@@ -75,20 +75,61 @@ public class Sorts{
 	return ans;
     }
 
-    public static void merge(int[] data, int startA, int endA, int startB, int endB){
-	
-    } 
-
-    public static void fillRandom(int[] ary){
-	for (int i = 0; i < ary.length; i++){
-	    ary[i] = (int)(Math.random() * 100);
+    public static void mergeSort(int[] arr){
+	if (arr.length == 1){
+	    return;
+	} 
+	int[] ans = mergeSortHelper(arr);
+	for (int i = 0; i < arr.length; i++){
+	    arr[i] = ans[i];
 	}
     }
+    
+    public static int[] mergeSortHelper(int[] arr){
+	int pos = arr.length/2;
+	int[] A = new int[pos];
+	int[] B = new int[arr.length - pos];
+	int p = 0;
+	for (int i = 0; i < A.length; i++){
+	    A[i] = arr[p];
+	    p++;
+	}
+	for (int i = 0; i < B.length; i++){
+	    B[i] = arr[p];
+	    p++;
+	}
+	if (pos == 0){
+	    return merge(A, B);
+	} else {
+	    return merge(mergeSortHelper(A), mergeSortHelper(B));
+	}	
+    }
 
-    public static void swap(int[] ary, int target, int rep){
-	int temp = ary[rep];
-	ary[rep] = ary[target];
-	ary[target] = temp;
+    /*
+    public static void merge(int[] data, int startA, int endA, int startB, int endB){
+	int[] temp = new int[endB-startA+1];
+	int pos = 0;
+	for (int i = startA; i <= endA; i++){
+	    temp[pos] = data[i];
+	    pos++;
+	}
+	for (int i = startB; i <= endB; i++){
+	    temp[pos] = data[i];
+	    pos++;
+	}
+	pos = 0;
+	for (int i = startA; i <= endB; i++){
+	    data[i] = temp[pos];
+	    pos++;
+	}
+	System.out.println(Arrays.toString(temp));
+	}*/ 
+
+    public static void main(String[]args){
+	int[] test = {101,9223,3823232,237323,0,-6,-100,1,36,25,34,332323,32,1};
+      	System.out.println(Arrays.toString(test));
+	mergeSort(test);
+	System.out.println(Arrays.toString(test));
     }
     
 }
