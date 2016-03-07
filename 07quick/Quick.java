064c94d12d5a148cb1646ed9adf4edbb74e195c6
Roy Kim <rkim8@stuy.edu>
2016-03-07 15:57:19 -0500

    I have an idea...

--- 07quick/Quick.java
+++ 07quick/Quick.java
@@ -1,5 +1,13 @@
+import java.util.Random;
 public class Quick{
     private static int partition(int[] data, int left, int right){
+	Random rand = new Random();
+	int p = left + nextInt(right-left);
+	for (int i = 0; i < data.length; i++){
+	    if (data[i] < p){
+		
+	    }
+	}
     }
 
     public static int quickselect(int[] data, int k){
