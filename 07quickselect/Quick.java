c3b0ee723ee3ecfaf3236fda9984f32a52ba5512
Roy Kim <rkim8@stuy.edu>
2016-03-09 03:51:16 -0500

    completed

--- 07quickselect/Quick.java
+++ 07quickselect/Quick.java
@@ -1,15 +1,43 @@
+//Credits to Wikipedia for helping me with the algorithim
 import java.util.Random;
 public class Quick{
-    private static int partition(int[] data, int left, int right){
-	Random rand = new Random();
-	int p = left + nextInt(right-left);
-	for (int i = 0; i < data.length; i++){
-	    if (data[i] < p){
-		
-	    }
-	}
+    private static void swap(int[] ary, int target, int rep){
+	int temp = ary[rep];
+	ary[rep] = ary[target];
+	ary[target] = temp;
     }
 
-    public static int quickselect(int[] data, int k){
+    private static int partition(int[] data, int left, int right){
+	Random rand = new Random();
+	int pivot = left + rand.nextInt(right-left);
+	int pivotVal = data[pivot];
+	swap(data, pivot, right);
+	int pos = left;
+	for (int i = left; i < right; i++){
+	    if (data[i] < pivotVal){
+		swap(data, pos, i);
+		pos++;
+	    } 
+	}
+	swap(data, right, pos);
+	return pos;
+    }
+    
+    private static int quickselect(int[] data, int k){
+	return quickselect(data, k, 0, data.length-1);
+    }
+
+    private static int quickselect(int[] data, int k, int left, int right){
+	if (left == right){
+	    return data[left]; 
+	}
+	int pivot = partition(data, left, right);
+	if (k == pivot){
+	    return data[k];
+	} else if (k < pivot){
+	    return quickselect(data, k, left, pivot-1);
+	} else {
+	    return quickselect(data, k, pivot+1, right);
+	}
     }
 }
