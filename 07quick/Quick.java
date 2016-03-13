ce422e10ed3b74ae8209ef8b8c9076d13c7acaa9
Roy Kim <rkim8@stuy.edu>
2016-03-13 14:04:40 -0400

    Completed new version of quickSort

--- 07quick/Quick.java
+++ 07quick/Quick.java
@@ -1,7 +1,7 @@
 //Credits to Mr. K and Wiki 
 import java.util.Arrays;
 public class Quick{
-    static boolean DEBUG = false;
+    static boolean DEBUG = true;
 
     public static String name(){
 	return "7,Kim,Roy";
@@ -19,48 +19,46 @@ public class Quick{
 	ary[target] = temp;
     }
 
-    private static int partition(int[] data, int left, int right){
-	int pivot = left + (int)(Math.random()*(right - left + 1)); 
+    private static int[] partition(int[] data, int left, int right){
+	int pivot = left + (int)(Math.random()*(right - left + 1));
+	int[] temp = new int[right - left + 1];
+	int[] limits = new int[2];
 	int pivotVal = data[pivot];
-	swap(data, pivot, right);
-	int pos = left;
-	for (int i = left; i < right; i++){
+	int less = 0;
+	int more = temp.length - 1;
+	for (int i = left; i < right + 1; i++){
 	    if (data[i] < pivotVal){
-		swap(data, pos, i);
-		pos++;
-	    } 
+		temp[less] = data[i];
+		less++;
+	    } else if (data[i] > pivotVal){
+		temp[more] = data[i];
+		more--;
+	    }
 	}
-	swap(data, right, pos);
-	return pos;
-    }
-    
-    public static int quickselect(int[] data, int k){
-	return quickselect(data, k, 0, data.length-1);
-    }
-
-    private static int quickselect(int[] data, int k, int left, int right){
-	if (left == right){
-	    return data[left]; 
+	for (int i = less; i < more +1; i++){
+	    temp[i] = pivotVal;
 	}
-	int pivot = partition(data, left, right);
-	if (k == pivot){
-	    return data[k];
-	} else if (k < pivot){
-	    return quickselect(data, k, left, pivot-1);
-	} else {
-	    return quickselect(data, k, pivot+1, right);
+	int j = 0;
+	for (int i = left; i < right+1; i++){
+	    data[i] = temp[j];
+	    j++;
 	}
+	limits[0] = left + less - 1;
+	limits[1] = left + more + 1;
+	return limits;
     }
-
+   
     public static void quickSort(int[] data){
 	quickSort(data, 0, data.length-1);
     }
     
     private static void quickSort(int[] data, int left, int right){
 	if (right > left){
-	    int pivot = partition(data, left, right);
-	    quickSort(data, left, pivot);
-	    quickSort(data, pivot + 1, right);
+	    int[] pivot = partition(data, left, right);
+	    int less = pivot[0];
+	    int more = pivot[1];
+	    quickSort(data, left, less);
+	    quickSort(data, more, right);
 	}
     }
 
@@ -80,32 +78,32 @@ public class Quick{
     }
     
     public static int quickselectOld(int[] data, int k){
-	return quickselect(data, k, 0, data.length-1);
+	return quickselectOld(data, k, 0, data.length-1);
     }
 
     private static int quickselectOld(int[] data, int k, int left, int right){
 	if (left == right){
 	    return data[left]; 
 	}
-	int pivot = partition(data, left, right);
+	int pivot = partitionOld(data, left, right);
 	if (k == pivot){
 	    return data[k];
 	} else if (k < pivot){
-	    return quickselect(data, k, left, pivot-1);
+	    return quickselectOld(data, k, left, pivot-1);
 	} else {
-	    return quickselect(data, k, pivot+1, right);
+	    return quickselectOld(data, k, pivot+1, right);
 	}
     }
 
     public static void quickSortOld(int[] data){
-	quickSort(data, 0, data.length-1);
+	quickSortOld(data, 0, data.length-1);
     }
     
     private static void quickSortOld(int[] data, int left, int right){
 	if (right > left){
-	    int pivot = partition(data, left, right);
-	    quickSort(data, left, pivot);
-	    quickSort(data, pivot + 1, right);
+	    int pivot = partitionOld(data, left, right);
+	    quickSortOld(data, left, pivot);
+	    quickSortOld(data, pivot + 1, right);
 	}
     }
 
@@ -113,11 +111,13 @@ public class Quick{
 	int[] a = new int[4000000];
 	int[] b = new int[a.length];
 	for (int i = 0; i < a.length; i++){
-	    a[i] = (int)(Math.random() * 3); //Integer.MAX_VALUE);
+	    a[i] = (int)(((Math.random() * 4))); //2) - 1)* Integer.MAX_VALUE);
 	    b[i] = a[i];
-	}
-	//quickSort(a);
-	Arrays.sort(a);
-	//System.out.println("Done: Sorted="+Arrays.equals(a,b));
+	}	
+	quickSort(a);
+	//Arrays.sort(a);
+	//quickSortOld(a);
+	//System.out.println("Done: Sorted="+Arrays.equals(a,b));	
+	//System.out.println(Arrays.toString(a));
     }
 }
