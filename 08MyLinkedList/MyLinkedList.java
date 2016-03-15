e39c76af1818d5700e8af349ed0e82391e4ddac1
Roy Kim <rkim8@stuy.edu>
2016-03-15 12:57:59 -0400

    Started Linked Lists

---
+++ 08MyLinkedList/MyLinkedList.java
@@ -0,0 +1,36 @@
+public class MyLinkedList{
+    private LNode start;
+    private LNode head;
+    private int size;
+
+    int get(int index){
+	
+    }
+
+    int set(int index, int newValue){
+    }
+
+    int size(){
+    }
+    
+    int remove(int index){
+    }
+
+    boolean add(int index, int value){
+    }
+
+    boolean add(int value){
+    
+    }
+
+    int indexOf(int value){
+    
+    }
+
+    String toString(){
+    
+    }
+
+    public class LNode{
+    }
+}
