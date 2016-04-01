312b28e2f0b257bd04711bcf6be87cf932318422
Roy Kim <rkim8@stuy.edu>
2016-04-01 12:59:06 -0400

    Created file

---
+++ 10deque/MyDeque.java
@@ -0,0 +1,25 @@
+public class MyDeque{
+    public MyDeque<T>{
+    }
+
+    public void addFirst(T value){
+	
+    }
+    
+    public void addLast(T value){
+	
+    }
+
+    public T removeFirst(){
+    
+    }
+    
+    public T removeLast(){
+    }
+
+    public T getFirst(){
+    }
+
+    public T getLast(){
+    }
+}
