5f39e4a6555725cd84d790e36a823e6b2873f6ee
Roy Kim <rkim8@stuy.edu>
2016-04-05 00:36:10 -0400

    Wrote addFirst and addLast. No resize() yet.

--- 10deque/MyDeque.java
+++ 10deque/MyDeque.java
@@ -1,16 +1,38 @@
 public class MyDeque{
+    private int start, end;
     private int size;
-    private Object[] queue;
+    private T[] data;
 
     public MyDeque<T>{
+	data = (T[]) new Object[10];
+    }
+    
+    private void resize(){
+    
     }
 
     public void addFirst(T value){
-	
+	if (size == data.length){
+	    resize();
+	} 
+	if (start < 0){
+	    start = data.length - 1;
+	}
+	data[start] = value;
+	start--;
+	size++;
     }
     
     public void addLast(T value){
-	
+	if (size == data.length){
+	    resize();
+	} 
+	if (end = data.length){
+	    end = 0;
+	}
+	data[end] = value;
+	end++;
+	size++;
     }
 
     public T removeFirst(){
