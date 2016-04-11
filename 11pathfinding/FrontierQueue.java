0bec7c8627252d1ce8801db336c54fd18ad70f13
Roy Kim <rkim8@stuy.edu>
2016-04-11 05:31:51 -0400

    FrontierQueue

--- 11pathfinding/FrontierQueue.java
+++ 11pathfinding/FrontierQueue.java
@@ -1,2 +1,19 @@
+import java.util.*;
 public class FrontierQueue<T> implements Frontier<T>{
+    Queue<T> FrontierQueue = new ArrayDeque<T>();
+
+    public void add(T element){
+	FrontierQueue.add(element);
+    }
+
+    public T next(){
+	if (FrontierQueue.isEmpty()){
+	    throw new NoSuchElementException();
+	}
+	return FrontierQueue.remove();
+    }
+
+    public boolean hasNext(){
+	return !(FrontierQueue.size() == 0);
+    }
 }
