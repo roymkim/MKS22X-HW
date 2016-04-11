a930eeb375bc25d7d559d4037530d83a623dde3c
Roy Kim <rkim8@stuy.edu>
2016-04-11 04:59:26 -0400

    Completed FrontierStack

--- 11pathfinding/FrontierStack.java
+++ 11pathfinding/FrontierStack.java
@@ -1,3 +1,20 @@
+import java.util.*;
 public class FrontierStack<T> implements Frontier<T>{
+    Stack<T> FrontierStack = new Stack<T>();
+    
+    public void add(T element){
+	FrontierStack.push(element);
+    }
+    
+    public T next(){
+	if (FrontierStack.isEmpty()){
+	    throw new NoSuchElementException();
+	}
+	return FrontierStack.pop();
+    }
+
+    public boolean hasNext(){
+	return !FrontierStack.isEmpty();
+    }
 }
 
