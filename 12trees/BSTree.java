346bbf52cf87acdaa7792d66c8f136f1c040e89f
Roy Kim <rkim8@stuy.edu>
2016-04-20 13:13:31 -0400

    Started file

---
+++ 12trees/BSTree.java
@@ -0,0 +1,25 @@
+public class BSTree implements Comparable<Name>{
+    private class TreeNode{
+    }
+
+    private TreeNode<T> root;
+    
+    public BSTree(){
+	root = null;
+    }
+    
+    public int compareTo(Name n){
+    }
+    
+    public void add(T value){
+    }
+
+    //public String toString(){
+    //}
+
+    public boolean contains(T value){
+    }
+
+    public int getHeight(){
+    }
+}
