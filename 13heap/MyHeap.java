1a336c9707ae35d40ab80d74e2fb21ef5c5a38cd
Roy Kim <rkim8@stuy.edu>
2016-05-09 18:04:41 -0400

    Wrote First two constructors, doubleSize and peek

--- 13heap/MyHeap.java
+++ 13heap/MyHeap.java
@@ -3,34 +3,77 @@ import java.util.*;
 public class MyHeap<T extends Comparable<T>>{
     private int size;
     private T[] data;
+    private boolean isMax;
     
-    public MyHeap()
-	public MyHeap(T[] array)
-	
-	/**pushDown
-	   precondition: datas[k]'s children are valid heaps
-      postconditions:-the element at index k has been 
-      shifted to the correct spot.
-      -data[k] and is a valid heap
-	**/
-	private void pushDown(int k)
-	
-	/**pushUp
-      precondition: data is a heap with at most one item
-      out of place (element at k)
-      postconditions:-the element at index k has been 
-      shifted to the correct spot.
-      -data is a valid heap
-	**/
-	private void pushUp(int k)
-	private void heapify()
-	public T delete()
-	public void add(T x)
-	private void doubleSize()
-	public String toString()
-	
-	//do this last
-	public MyHeap(boolean isMax)
-	public MyHeap(T[] array, boolean isMax)
-	
+    public MyHeap(){
+	data = (T[])(new Comparable[10]);
+	isMax = true;
+	size = 0;
+    }
+    
+    public MyHeap(T[] array){
+	data = array;
+	isMax = true;
+	size = array.length;
+    }
+    
+    /**pushDown
+       precondition: datas[k]'s children are valid heaps
+       postconditions:-the element at index k has been 
+       shifted to the correct spot.
+       -data[k] and is a valid heap
+    **/
+    private void pushDown(int k){
+    }
+    
+    /**pushUp
+       precondition: data is a heap with at most one item
+       out of place (element at k)
+       postconditions:-the element at index k has been 
+       shifted to the correct spot.
+       -data is a valid heap
+    **/
+    private void pushUp(int k){
+    }
+    
+    private void heapify(){
+    }
+    
+    public T delete(){
+    }
+
+    public T peek(){
+	if (size == 0){
+	    throw new NoSuchElementException();
+	} else {
+	    return data[1];
 	}
+    }
+    
+    public void add(T x){
+    }
+    
+    private void doubleSize(){
+	T[] temp = (T[])(new Comparable[data.length*2+1]);
+	for (int i = 1; i <= size; i++){
+	    temp[i] = data[i];
+	}
+	data = temp;
+    }
+    
+    public String toString(){
+	String temp = "";
+	for (int i = 1;i <= size;i++){
+	    temp+=data[i]+" ";
+	}
+	return temp;
+    }
+    
+    //do this last
+    public MyHeap(boolean isMax){
+    }
+    
+    public MyHeap(T[] array, boolean isMax){
+    }
+    
+}
