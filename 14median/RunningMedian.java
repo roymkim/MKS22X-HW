17565b5d180ade9960ddcdef556fce6290a63a1a
Roy Kim <rkim8@stuy.edu>
2016-05-10 13:07:04 -0400

    bloop

--- 14median/RunningMedian.java
+++ 14median/RunningMedian.java
@@ -1,13 +1,17 @@
 public class RunningMedian{
+    T[] data;
+    
     public RunningMedian(){
-	
     }
 
     public int getMedian(){
-
+	if (size == 0){
+	    throw new NoSuchElementException();
+	}
+	return;
     }
 
     public int add(Integer i){
-	
+	return;
     }
 }
